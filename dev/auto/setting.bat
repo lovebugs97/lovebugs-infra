3c373c493a383ba9b51c485d357d1d6e3a6f4876
rivercity310 <h970126@gmail.com>
2024-07-24 03:56:37 +0900

    feat: nginx 컨테이너 추가 구성

--- dev/auto/setting.bat
+++ dev/auto/setting.bat
@@ -2,5 +2,6 @@
 
 CALL docker-compose -f ../docker/docker-compose-local.yaml down
 CALL copy "./vault.json" "../docker/mount/vault/config"
+CALL copy "./nginx.conf" "../docker/mount/nginx/conf"
 CALL docker-compose -f ../docker/docker-compose-local.yaml up -d
 CALL docker exec -it rabbitmq rabbitmq-plugins enable rabbitmq_management
